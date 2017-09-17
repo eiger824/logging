be6659a3b8edf5b560cee78f24e7cc8435b0c6f1
Santi Pagola <santipagola@gmail.com>
2017-09-17 13:08:41 +0200

    First commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+print: main.c log.h
+	gcc main.c -o print
+
+clean:
+	rm -f print
---
+++ main.c
@@ -0,0 +1,10 @@
+#include "log.h"
+
+int main()
+{
+	log_info("Hello there!");
+	log_info("Age: %d", 24);
+	log_info("Age %s");
+	log_info("Hello world! My name is [%s], I am %d years old and my first initial is %c", "Santi", 24, 'S');
+	return 0;
+}
